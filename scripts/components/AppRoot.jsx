9fdc06c0466132cf6aebce56f84ad3ecea7ff8d3
timnew <timnew.wti@gmail.com>
2015-05-21 21:24:48 +0800

    Branding the app

--- scripts/components/AppRoot.jsx
+++ scripts/components/AppRoot.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, CollapsibleNav, Nav, NavItem} from 'react-bootstrap';
 import { NavItemLink } from 'react-router-bootstrap';
 import { RouteHandler } from 'react-router';
 
@@ -9,11 +9,16 @@ class AppRoot extends React.Component {
   render() {
     return (
       <div>
-        <Navbar brand="Werewolves" inverse fixedTop toggleNavKey={'navbar'}>
-          <Nav eventKey={'navbar'}>
-            <NavItemLink to="Setup">Setup Game</NavItemLink>
-            <NavItemLink to="Play">Play Game</NavItemLink>
-          </Nav>
+        <Navbar brand="Werewolves App" inverse fixedTop toggleNavKey={'navbar'}>
+          <CollapsibleNav eventKey={'navbar'}>
+            <Nav navbar eventKey={'navbar'}>
+              <NavItemLink to="Setup">Setup Game</NavItemLink>
+              <NavItemLink to="Play">Play Game</NavItemLink>
+            </Nav>
+            <Nav navbar right>
+              <NavItem href='http://timnew.me'>By TimNew & his pals</NavItem>
+            </Nav>
+          </CollapsibleNav>
         </Navbar>
         <RouteHandler/>
       </div>
